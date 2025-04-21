a36948f56e1e58b56ff438cfced31f4d3024c3dd
anikaT19 <anika19tabassum@gmail.com>
2025-04-21 18:30:16 +0600

    comit55

---
+++ src/main/java/com/examplecclass/teaaestate/FcEnterMonthlyBudgetAllocationController.java
@@ -0,0 +1,30 @@
+package com.examplecclass.teaaestate;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FcEnterMonthlyBudgetAllocationController
+{
+    @javafx.fxml.FXML
+    private Label allertLabel;
+    @javafx.fxml.FXML
+    private TextField mbaTransporttextfield;
+    @javafx.fxml.FXML
+    private TextField mbaRawMaterialstextfield;
+    @javafx.fxml.FXML
+    private DatePicker mbaPeriodDatePicker;
+    @javafx.fxml.FXML
+    private TextField mbaSubmittedtextfield;
+    @javafx.fxml.FXML
+    private TextField mbaLabourtextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void mbaAllocateButtonOnAction(ActionEvent actionEvent) {
+    }
+}
